1bbda785c35d7ba03d9c90b20781d9c4ef515102
Cesar Cerda <cesarcerdacomputer@gmail.com>
2026-03-26 13:19:17 -0600

    Felizes Vacaciones

--- CapaDatos/CDProducto.cs
+++ CapaDatos/CDProducto.cs
@@ -9,7 +9,7 @@ using System.Text;
 namespace CapaDatos
 {
     public class CDProducto
-    {
+    { 
         SqlConnection conexion;
         //metodo para obtener la Lista de todos los productos
         public List<Producto> ListarProducto()
